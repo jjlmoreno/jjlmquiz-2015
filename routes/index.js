d8251d1880c6d4b50fa6372cbd6e6fcdbe09fb8a
Jose Lopez <jjlmoreno@gmail.com>
2015-08-18 12:19:51 +0200

    Comentarios en quizes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,7 +11,7 @@ router.get('/', function(req, res) {
 
 /* GET Author page. */
 router.get('/author', function(req, res) {
-  res.render('author', { title: 'Author' });
+  res.render('author', { title: 'Author'});
 });
 
 // Autoload de comandos con :quizId
@@ -25,5 +26,8 @@ router.get('/quizes/:quizId(\\d+)/edit',	quizController.edit);
 router.put('/quizes/:quizId(\\d+)',			quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
 
+
+router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
 
 module.exports = router;
